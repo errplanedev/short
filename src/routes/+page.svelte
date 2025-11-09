6cbb123ebcf4a6a2e5c96897c33c5bec1623bec3
errplanedev <110797323+errplanedev@users.noreply.github.com>
2025-11-08 17:43:07 -0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>short</title>
+    <meta name="description" content="shorten stuff">
+</svelte:head>
+
+<main>
+    <h1>short</h1>
+    <p>shortener</p>
+    <h2>how to use</h2>
+    <code>https://short.dour.com/post/postId</code>
+    <br>
+    <a href="/post/8812470" target="_blank">try it</a>
+</main>
